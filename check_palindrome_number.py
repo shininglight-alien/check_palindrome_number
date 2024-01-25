de82ac11ae6d381c4d7634520f67d4060363b9ff
Ellaine Rivera <ellainejoycerrivera@iskolarngbayan.pup.edu.ph>
2024-01-25 12:39:00 +0800

    it is not working, haha

--- check_palindrome_number.py
+++ check_palindrome_number.py
@@ -1,5 +1,11 @@
 # Check Palindrome Number
 
 def is_palindrome(num):
-    return str(num) == str(num)[::-1]
+    num = int(input("Enter a number: "))
+    return str(num) == str(num)[ ::-1]
+
+if is_palindrome():
+    print("Yes. The given is a palindrome number.")
+else:
+    print("No. The given is not a palindrome number.")
 
