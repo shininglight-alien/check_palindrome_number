9c6fd58a91ce5e423258a85d8ac46017c7bbbbda
Ellaine Rivera <ellainejoycerrivera@iskolarngbayan.pup.edu.ph>
2024-01-25 12:34:08 +0800

    not final

--- check_palindrome_number.py
+++ check_palindrome_number.py
@@ -3,14 +3,3 @@
 def is_palindrome(num):
     return str(num) == str(num)[::-1]
 
-num = 121
-if is_palindrome(num):
-    print(f"Original number {num}\nYes. Given number is a palindrome number")
-else:
-    print(f"Original number {num}\nNo. Given number is not a palindrome number")
-
-num = 125
-if is_palindrome(num):
-    print(f"Original number {num}\nYes. Given number is a palindrome number")
-else:
-    print(f"Original number {num}\nNo. Given number is not a palindrome number")
